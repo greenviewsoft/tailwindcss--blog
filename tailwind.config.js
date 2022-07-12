d95f863465a2ba28e4483bc6be242bd2ff9522e9
Md Tipu Sultan <greenviewsoft@gmail.com>
2022-07-12 17:17:06 +0600

    blog project navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'poppins' :['Poppins'],
+        'roboto' :['Roboto'],
+      }
+    },
+  },
+  plugins: [],
+}
